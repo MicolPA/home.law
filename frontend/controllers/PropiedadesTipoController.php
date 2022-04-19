1fdfb1a2bb0c8a56ff142c26f8192f6b99a3c2d9
Micol <micolpa08@gmail.com>
2022-04-18 21:49:51 -0400

    Update PropiedadesTipoController.php

--- frontend/controllers/PropiedadesTipoController.php
+++ frontend/controllers/PropiedadesTipoController.php
@@ -72,7 +72,8 @@ class PropiedadesTipoController extends Controller
 
         if ($this->request->isPost) {
             if ($model->load($this->request->post()) && $model->save()) {
-                return $this->redirect(['view', 'id' => $model->id]);
+                return $this->redirect(['index']);
+                // return $this->redirect(['view', 'id' => $model->id]);
             }
         } else {
             $model->loadDefaultValues();
@@ -95,7 +96,8 @@ class PropiedadesTipoController extends Controller
         $model = $this->findModel($id);
 
         if ($this->request->isPost && $model->load($this->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
+                return $this->redirect(['index']);
+            // return $this->redirect(['view', 'id' => $model->id]);
         }
 
         return $this->render('update', [
