02573292766aeb94d321a6ca3eab30e9a023f52b
Micol Peralta <micolpa08@gmail.com>
2022-07-06 19:31:31 -0400

    Update _modal_oferta_rechazadas.php

--- frontend/views/propiedades/_modal_oferta_rechazadas.php
+++ frontend/views/propiedades/_modal_oferta_rechazadas.php
@@ -10,15 +10,15 @@ $servicios = new \common\models\Servicios();
         <a class="text-danger"></a>
         <button type="button" class="text-end text-danger fw-bold float-end bg-white border-0" data-bs-dismiss="modal">CERRAR</button>
       </div>
-      <div class="modal-body px-4">
+      <div class="modal-body px-5">
         <h5 class="text-center text-primary mb-5">OFERTAS RECHAZADAS</h5>
         <hr>
         <?php foreach ($ofertasRechazadas as $oferta): ?>
           <div class="row">
-            <div class="col-md-6 text-center">
+            <div class="col-md-6 text-center pt-3">
               <p style="font-size:18px">USD$<?= number_format($oferta->monto) ?></p>
             </div>
-            <div class="col-md-6">
+            <div class="col-md-6 pt-3">
               <p style="font-size:18px"><?= $servicios->formatDate($oferta->status_updated_date) ?></p>
             </div>
           </div>
