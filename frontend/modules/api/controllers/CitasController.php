ecb154ac70c1e19eebc5e84c3355dfe74ab246dd
Micol Peralta <micolpa08@gmail.com>
2022-07-25 19:00:10 -0400

    Update CitasController.php

--- frontend/modules/api/controllers/CitasController.php
+++ frontend/modules/api/controllers/CitasController.php
@@ -16,13 +16,13 @@ class CitasController extends \yii\web\Controller
     public function actionGetAgentes(){
 
         Yii::$app->response->format = Response:: FORMAT_JSON; 
-        
+
         $agentes_all = array();
         $agentes = \frontend\models\User::find()->where(['role_id' => 2])->all();
 
         foreach ($agentes as $agente) {
             $agentes_all[] = array(
-                'id' => $agente->id
+                'id' => $agente->id,
                 'first_name' => $agente->first_name,
                 'last_name' => $agente->last_name,
                 'photo_url' => $agente->photo_url,
